a96b7f0db6fab68ad68ad5b5b1c1013170ba2899
Jack Reilly <jackdreilly@gmail.com>
2022-12-15 18:24:05 +0100

    gcloud deploy

--- Parameters.py
+++ Parameters.py
@@ -1,17 +1,25 @@
-piping_embodied = 240 #MJ/m (diameter = 100 mm)
-piping_cost = 500 # $/m ###CHECK Bradshaw
-pipe_lifetime = 25 #years
-water_weight = 9.8 #KN/m3
-wastewater_demand_residential = 0.19 #m3/person-day http://www.sfwater.org/modules/showdocument.aspx?documentid=6543
-wastewater_demand_commercial = 0.12 #m3/person-day  http://www.sfwater.org/modules/showdocument.aspx?documentid=6543 with 600,000 employees
-npr_percent_residential = 0.5  # http://ggashrae.org/images/meeting/021116/paula_kehoe_ggashrae_02_11_2016.pdf
-npr_percent_commercial = 0.95 # http://ggashrae.org/images/meeting/021116/paula_kehoe_ggashrae_02_11_2016.pdf
-npr_water_demand_residential =  wastewater_demand_residential*npr_percent_residential#m3/person-day
-npr_water_demand_commercial = wastewater_demand_commercial*npr_percent_commercial #m3/person-day
-lifetime_WWTP = 30 #years Eggimann connection rates
+piping_embodied = 240  # MJ/m (diameter = 100 mm)
+piping_cost = 500  # $/m ###CHECK Bradshaw
+pipe_lifetime = 25  # years
+water_weight = 9.8  # KN/m3
+wastewater_demand_residential = (
+    0.19  # m3/person-day http://www.sfwater.org/modules/showdocument.aspx?documentid=6543
+)
+wastewater_demand_commercial = 0.12  # m3/person-day  http://www.sfwater.org/modules/showdocument.aspx?documentid=6543 with 600,000 employees
+npr_percent_residential = (
+    0.5  # http://ggashrae.org/images/meeting/021116/paula_kehoe_ggashrae_02_11_2016.pdf
+)
+npr_percent_commercial = (
+    0.95  # http://ggashrae.org/images/meeting/021116/paula_kehoe_ggashrae_02_11_2016.pdf
+)
+npr_water_demand_residential = (
+    wastewater_demand_residential * npr_percent_residential
+)  # m3/person-day
+npr_water_demand_commercial = wastewater_demand_commercial * npr_percent_commercial  # m3/person-day
+lifetime_WWTP = 30  # years Eggimann connection rates
 pump_efficiency = 0.45
-in_builing_piping_sf = 0.05 #m/m2 #Hasik LCA building
-electricity_cost = 0.12 #$/kWh EIA
-US_electricity_GHG = 0.59 #kgCO2/kWh (EIA for electricity mix and LCA emission factors Stokes and Horvath http://uc-ciee.org/downloads/Life-cycleHorvath.pdf)
-treatment_embodied_GHG = 0.06 #kgCO2/m3 Kavvada
-treatment_embodied_energy = 0.3 #kWh/m3 Kavvada
+in_builing_piping_sf = 0.05  # m/m2 #Hasik LCA building
+electricity_cost = 0.12  # $/kWh EIA
+US_electricity_GHG = 0.59  # kgCO2/kWh (EIA for electricity mix and LCA emission factors Stokes and Horvath http://uc-ciee.org/downloads/Life-cycleHorvath.pdf)
+treatment_embodied_GHG = 0.06  # kgCO2/m3 Kavvada
+treatment_embodied_energy = 0.3  # kWh/m3 Kavvada
--- app.py
+++ app.py
@@ -1,39 +1,52 @@
 from __future__ import print_function
-from sys import stderr
-# steps:
-# install flask
-# pip install flask
-# Run server:
-# FLASK_DEBUG=1 FLASK_APP=server.py flask run
+import os
+
 import flask
 from flask import Flask, request
-from optimization2 import *
 
+from optimization2 import *
 
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route("/")
 def root():
-  return app.send_static_file('index.html')
+    return app.send_static_file("index.html")
+
 
 @app.route("/lat_lng")
 def lat_lng():
-  lat = float(request.args.get('lat'))
-  lng = float(request.args.get('lng'))
-  metric = request.args.get('metric')
-  a = float(request.args.get('a'))
-  b = float(request.args.get('b'))
-  c = float(request.args.get('c'))
-  d = float(request.args.get('d'))
-  direct = float(request.args.get('direct'))
-  ghg = float(request.args.get('electricity_GHG'))
+    lat = float(request.args.get("lat"))
+    lng = float(request.args.get("lng"))
+    metric = request.args.get("metric")
+    a = float(request.args.get("a"))
+    b = float(request.args.get("b"))
+    c = float(request.args.get("c"))
+    d = float(request.args.get("d"))
+    direct = float(request.args.get("direct"))
+    ghg = float(request.args.get("electricity_GHG"))
 
-  return flask.jsonify(getMyGeoJSON(lat=lat, lng=lng, path='document.csv', metric=metric, a=a, b=b, c=c, d=d, direct=direct, ghg=ghg))
+    return flask.jsonify(
+        getMyGeoJSON(
+            lat=lat,
+            lng=lng,
+            path="document.csv",
+            metric=metric,
+            a=a,
+            b=b,
+            c=c,
+            d=d,
+            direct=direct,
+            ghg=ghg,
+        )
+    )
 
-def getMyGeoJSON(lat,lng, path, metric, a, b, c, d, direct, ghg):
-  points = getServiceArea((lat,lng), path, metric,  a, b, c, d, direct, ghg)
-  return points
 
-if __name__ == '__main__':
-  port = int(os.environ.get('PORT', 5000))
-  app.run(host='0.0.0.0', port=port, debug=True)
+def getMyGeoJSON(lat, lng, path, metric, a, b, c, d, direct, ghg):
+    points = getServiceArea((lat, lng), path, metric, a, b, c, d, direct, ghg)
+    return points
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5001))
+    app.run(host="0.0.0.0", port=port, debug=False)
